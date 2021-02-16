5feb0b5be75303beac2bc9bdddede69946833024
gw8ndal <61947142+gw8ndal@users.noreply.github.com>
2021-02-16 21:36:56 +0100

    Supprimé le 3e chiffre après la virgule

--- countdown_timer.py
+++ countdown_timer.py
@@ -2,7 +2,7 @@ import time
 
 def countdown():
     for i in range(0,1):
-        tmp = 10000
+        tmp = 1000
         try:
             when_to_stop = abs(int(tmp))
         except KeyboardInterrupt:
@@ -10,14 +10,11 @@ def countdown():
         except:
             print('Not a number')
         while when_to_stop > -1:
-            s, ms = divmod(when_to_stop, 1000)
-            time_left = str(s).zfill(2) + ':' + str(ms).zfill(3)
+            s, ms = divmod(when_to_stop, 100)
+            time_left = str(s).zfill(2) + ':' + str(ms).zfill(2)
             print(time_left + '\r', end = '')
             time.sleep(0.001)
             when_to_stop -= 1
     return time_left
 
 print(countdown())
-
-
-    
